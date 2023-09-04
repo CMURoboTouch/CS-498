90940d98f00fe0cefa7144ca159000cdd6acc445
Arpit Agarwal <arpit15945@gmail.com>
2023-09-04 19:05:28 -0400

    tut1/ex2 num channels check

--- tutorial1/exercise2/main.py
+++ tutorial1/exercise2/main.py
@@ -1,10 +1,12 @@
 import mitsuba as mi
 mi.set_variant("scalar_rgb")
 # load scene
-scene = mi.load_file("cbox/cbox_init.xml")
+scene = mi.load_file("cbox/cbox_final.xml")
 # calculate image
 image = mi.render(scene, spp=256)
 # extract normals
+if image.shape[2] < 6:
+    raise Exception("Expected at least 6 channels in the image!")
 normals = image[:,:,3:6]
 # convert normals [-1,1] -> [0,1]
 normals = (normals+1.0)/2.0
