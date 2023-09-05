c5757b721b89e4656fc5a285def7d5c1afeac2b0
Arpit Agarwal <arpit15945@gmail.com>
2023-09-05 09:50:07 -0400

    hw1

---
+++ hw1/main.py
@@ -0,0 +1,8 @@
+import mitsuba as mi
+mi.set_variant("scalar_rgb")
+# load scene
+scene = mi.load_file("cbox/cbox_translatedx0.5.xml")
+# calculate image
+image = mi.render(scene, spp=256)
+# write image to computer
+mi.util.write_bitmap("cbox.png", image)
