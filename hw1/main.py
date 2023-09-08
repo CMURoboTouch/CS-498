0501c35d8a2d9a463ddddc590e6d773d163b0328
arpit agarwal <arpit15945@gmail.com>
2023-09-08 15:12:54 -0500

    remove spp from render script to take the value from xml

--- hw1/main.py
+++ hw1/main.py
@@ -3,6 +3,6 @@ mi.set_variant("scalar_rgb")
 # load scene
 scene = mi.load_file("cbox/cbox_translatedx0.5.xml")
 # calculate image
-image = mi.render(scene, spp=256)
+image = mi.render(scene)
 # write image to computer
 mi.util.write_bitmap("cbox.png", image)
